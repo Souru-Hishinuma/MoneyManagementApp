ccfc4f782cd9bf9613ab3e6150ab55acaa1248f7
Souru-Hishinuma <sourunrun@gmail.com>
2022-06-28 00:54:05 +0900

    backend setup and register / login form

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const port = 5000;
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+mongoose.connect(process.env.MONGOURL)
+.then(() => {
+    console.log('接続中');
+}).catch((err) => {
+    console.log(err);
+})
+
+app.get('/', (req, res) => res.send('Hello, World!'));
+app.listen(port, () => console.log(`Node JS Server started at ${port}!`));
